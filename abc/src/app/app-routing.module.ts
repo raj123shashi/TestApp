052533fd834ce90358675bd4c8c9b9499f056a43
raj123shashi <39792479+raj123shashi@users.noreply.github.com>
2020-08-27 04:06:47 -0700

    ui

---
+++ abc/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+
+
+const routes: Routes = [
+  { path: 'hello', component: HelloWorldComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
